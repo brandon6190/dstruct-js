fb9f8717281de55a48b4b61d23ae97a1f7978d79
brandon6190 <brandon6190@outlook.com>
2019-02-19 13:05:47 -0500

    Created the boiler plate for class and methods, and exporting it

---
+++ src/BinarySearchTree.js
@@ -0,0 +1,36 @@
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+
+  add(value) {
+
+  }
+
+  contains(value) {
+
+  }
+
+  depthFirstPre(callback) {
+
+  }
+
+  depthFirstIn(callback) {
+
+  }
+
+  depthFirstPost(callback) {
+
+  }
+
+  breadthFirst(callback) {
+    
+  }
+
+}
+
+module.exports = {
+  BinarySearchTree
+};

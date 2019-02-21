6fff1635771983314173ea991b454db65f1a2805
brandon6190 <brandon6190@outlook.com>
2019-02-21 12:49:44 -0500

    created add() method for BST

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -6,7 +6,25 @@ class BinarySearchTree {
   }
 
   add(value) {
+    if (value > this.value) {
+      if (!this.right) {
+        const bst = new BinarySearchTree(value);
+        this.right = bst;
+      }
+      else {
+        this.right.add(value);
+      }
+    }
 
+    else {
+      if (!this.left) {
+        const bst = new BinarySearchTree(value);
+        this.left = bst;
+      }
+      else {
+        this.left.add(value);
+      }
+    }
   }
 
   contains(value) {

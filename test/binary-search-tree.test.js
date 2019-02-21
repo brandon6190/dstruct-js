8e6c2dbb2a7fa7514e70b88a67b602c8536a2b4c
brandon6190 <brandon6190@outlook.com>
2019-02-21 12:49:22 -0500

    created unit test for add() method

--- test/binary-search-tree.test.js
+++ test/binary-search-tree.test.js
@@ -1,7 +1,7 @@
 const { assert } = require('chai');
 const { BinarySearchTree } = require('../src/BinarySearchTree');
 
-describe('BinarySearchTree', () => {
+describe('BinarySearchTree()', () => {
   let binarySearchTree;
 
   beforeEach( () => {
@@ -15,5 +15,17 @@ describe('BinarySearchTree', () => {
     assert.equal(typeof binarySearchTree.depthFirstPre, 'function');
     assert.equal(typeof binarySearchTree.depthFirstPost, 'function');
     assert.equal(typeof binarySearchTree.breadthFirst, 'function');
+  });
+
+  describe('add() method', () => {
+    it('should add values at the correct location', () => {
+      binarySearchTree.add(2);
+      binarySearchTree.add(3);
+      binarySearchTree.add(7);
+      binarySearchTree.add(6);
+
+      assert.equal(binarySearchTree.left.right.value, 3);
+      assert.equal(binarySearchTree.right.left.value, 6);
+    });
   });
 });

98e895097c570e08e528ecb82c84ebf36d0b7932
brandon6190 <brandon6190@outlook.com>
2019-03-09 00:34:17 -0500

    created dequeue method for Queue

--- src/Queue.js
+++ src/Queue.js
@@ -10,7 +10,18 @@ class Queue {
   }
 
   dequeue() {
+    const element = this.storage[0];
 
+    if (!this.index) return undefined;
+
+    for (let i = 0; i < this.index; i += 1) {
+      this.storage[i] = this.storage[i+1];
+    }
+    
+    delete this.storage[this.index-1];
+    this.index--;
+
+    return element;
   }
 
 }

b1f0b56e93af18df223f7df904d1e2150c449c02
brandon6190 <brandon6190@outlook.com>
2019-03-09 00:19:29 -0500

    created test for enqueue method for Queue

--- test/queue.test.js
+++ test/queue.test.js
@@ -14,4 +14,9 @@ describe('Queue()', () => {
     assert.equal(typeof queue.enqueue, 'function');
     assert.equal(typeof queue.dequeue, 'function');
   });
+
+  it('should enqueue elements to end of queue', () => {
+    queue.enqueue(0);
+    assert.equal(queue.storage['0'], 0);
+  });
 });

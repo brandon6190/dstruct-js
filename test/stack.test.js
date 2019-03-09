074ea8661a682cbdd3808c5d31d597540dc0c924
brandon6190 <brandon6190@outlook.com>
2019-03-08 23:58:44 -0500

    created test for push method for Stack

--- test/stack.test.js
+++ test/stack.test.js
@@ -1,7 +1,7 @@
 const { assert } = require('chai');
 const { Stack } = require('../src/Stack');
 
-describe('Stack', () => {
+describe('Stack()', () => {
   let stack, storage;
 
   beforeEach(() => {
@@ -10,9 +10,15 @@ describe('Stack', () => {
   });
 
   it('should have a push and pop method', () => {
-    assert.equal(stack.hasOwnProperty('push'), true);
-    assert.equal(stack.hasOwnProperty('pop'), true);
+    assert.equal('push' in stack, true);
+    assert.equal('pop' in stack, true);
     assert.equal(typeof stack.push, 'function');
     assert.equal(typeof stack.pop, 'function');
+  });
+
+  it('should push elements to stack', () => {
+    stack.push(0);
+    stack.push(1);
+    assert.equal(stack.storage['0'], 0); 
   });
 });

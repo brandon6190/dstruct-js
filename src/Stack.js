1ac020f13e3627f52bed6fc4f05ed8aa640f8e72
brandon6190 <brandon6190@outlook.com>
2019-03-08 23:59:44 -0500

    created push method for Stack

---
+++ src/Stack.js
@@ -0,0 +1,19 @@
+class Stack {
+  constructor() {
+    this.storage = {};
+    this.index = 0;
+  }
+
+  push(value) {
+    this.storage[this.index] = value;
+    this.index++;
+  }
+
+  pop() {
+
+  }
+}
+
+module.exports = {
+  Stack
+};
